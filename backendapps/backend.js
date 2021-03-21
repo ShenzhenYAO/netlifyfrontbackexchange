f03397527b2246fb1d75cc95950f0be8e765211b
Shenzhen YAO <junk.them@hotmail.com>
2021-03-21 15:49:53 -0600

    20210321j

--- backendapps/backend.js
+++ backendapps/backend.js
@@ -1,38 +1,26 @@
 // to parse the string from frontend
-const qs = require('querystring');
-const bodyParser = require('body-parser');
 
-exports.handler = async (event, context) => { 
-
-    const { query } = qs.parse(event.body); 
-    let str2 = JSON.stringify({query});
+exports.handler = async (event, context, callback) => { 
     
-    const j3 = bodyParser.json(event.body);
-    let str3 = 'j3:' + JSON.stringify(j3)
+    const newpromise = new Promise(
+        (resolve) => {
+            resolve(event.body)
+        } // resolve
+    ); // new promise
 
-    
-    // console.log("the string from frontend:", strfromfront)
-
-    // const v2 = qs.parse(event.body);
-    // const v3 = JSON.stringify(v2)
-
-    // const strFromBackToFront = "The string from frontend '" 
-    //     + strfromfront + v2 + v3 
-    //     + JSON.stringify({strfromfront}) + "' was received. This message was from the backend";
-
-    // const lambdaResponse = {
-    //     statusCode: 200,
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: strFromBackToFront,
-    //   };
+    const resolveddata = await newpromise.then(d => {
+        return d
+    });
+    let str2 = JSON.stringify(resolveddata)
 
     // isn't it just like that????
     let thejson = event.body
     let thejsonstr = JSON.stringify(thejson)
-    let strFromBackToFront = str2 + "\n" + str3 + "\n" + "The string from frontend " 
-        + thejsonstr + " was received. This message was from the backend";
+    let strFromBackToFront = str2 + "\nThe string from frontend "  + thejsonstr 
+        + " was received. This message was from the backend." 
+
+    
+
 
     // return lambdaResponse
     return {
