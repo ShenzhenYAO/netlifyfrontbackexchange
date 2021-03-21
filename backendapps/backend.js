8e12ab7c7b5f9cf48dea94ee76a5c5981e4932b4
Shenzhen YAO <junk.them@hotmail.com>
2021-03-21 16:01:54 -0600

    20210321k

--- backendapps/backend.js
+++ backendapps/backend.js
@@ -1,31 +1,47 @@
 // to parse the string from frontend
+// import mongoclient module from mongodb
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_TREEDB_TREES_URI;
+const dbName = 'treedb', collectionName = 'trees';
 
-exports.handler = async (event, context, callback) => { 
-    
-    const newpromise = new Promise(
-        (resolve) => {
-            resolve(event.body)
-        } // resolve
-    ); // new promise
+exports.handler = async (event, context, callback) => {
 
-    const resolveddata = await newpromise.then(d => {
-        return d
-    });
-    let str2 = JSON.stringify(resolveddata)
+    // we keep the DB connection alive
+    context.callbackWaitsForEmptyEventLoop = false;
 
     // isn't it just like that????
     let thejson = event.body
     let thejsonstr = JSON.stringify(thejson)
-    let strFromBackToFront = str2 + "\nThe string from frontend "  + thejsonstr 
-        + " was received. This message was from the backend." 
+    let strFromBackToFront = "The string from frontend " + thejsonstr
+        + " was received. This message was from the backend."
 
-    
+    let resolveddata = {
+        userid: "randomone",
+        treejson: { name: "makeup", segment: 0, string: thejsonstr }
+    }
+
+    //make an instant (client) of the MongoClient module,  add {useUnifiedTopology: true} to suppress some warning messages...
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // use promise to add data to mongodb
+    const newpromise1 = new Promise(
+        // then new promise is to define a resolved value
+        (resolve) => {
+            // connect to mongodb and add a record
+            client.connect(err => {
+                const collection = client.db(dbName).collection(collectionName);
+                collection.insertOne(resolveddata, function (err, res) {
+                    if (err) throw err;
+                });
+            })// client.connect()
+        }//resolve
+    ) // new promise1;
 
 
     // return lambdaResponse
     return {
         statusCode: 200,
-        body: strFromBackToFront 
-      };
+        body: strFromBackToFront
+    };
 
 }
