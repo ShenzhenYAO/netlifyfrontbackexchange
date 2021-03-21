6c3f5ddea73148bb428099f039564e2323002358
Shenzhen YAO <junk.them@hotmail.com>
2021-03-21 14:31:57 -0600

    20210321b

--- backendapps/backend.js
+++ backendapps/backend.js
@@ -1,20 +1,25 @@
 // to parse the string from frontend
 const qs = require('querystring');
 
-module.exports.handler = async (event, context) => { 
+module.handler = async (event, context) => { 
 
     const { strfromfront } = qs.parse(event.body);    
     console.log("the string from frontend:", strfromfront)
 
-    const strFromBackToFront = "The string from frontend '" + strfromfront + "' was received. This message was from the backend";
+    const strFromBackToFront = "The string from frontend '" + JSON.stringify({strfromfront}) + "' was received. This message was from the backend";
 
-    const lambdaResponse = {
+    // const lambdaResponse = {
+    //     statusCode: 200,
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: strFromBackToFront,
+    //   };
+
+    // return lambdaResponse
+    return {
         statusCode: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: strFromBackToFront,
+        body: strFromBackToFront 
       };
 
-    return lambdaResponse
 }
